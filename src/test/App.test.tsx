bd13c3833e41869e077cb562615a5192f11748e5
martin Høva <martirho@stud.ntnu.no>
2024-09-30 15:59:24 +0200

    chore: initial commit
    co-authored-by: @adriahso @pettete

---
+++ src/test/App.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import App from "../App";
+import "@testing-library/jest-dom";
+
+describe("App", () => {
+    it("successfully loads api", async () => {
+        render(<App />);
+        expect(true).toBeTruthy();
+    });
+});
