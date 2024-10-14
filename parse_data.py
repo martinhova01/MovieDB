7898e4813f6b8a360b732adb53d8fe30ca694d35
Runar Saur Modahl <runarsm@stud.ntnu.no>
2024-10-14 15:22:35 +0200

    feat(scripts): update Python script to initialize MongoDB (#24)

--- parse_data.py
+++ parse_data.py
@@ -2,8 +2,11 @@
 
 import json
 import os
+import sys
 
 import pandas as pd
+import pymongo
+from tqdm import tqdm
 
 DOWNLOAD_DATA_URL = (
     "https://www.kaggle.com/datasets/asaniczka/tmdb-movies-dataset-2023-930k-movies"
@@ -97,23 +100,64 @@ def get_all_movies(df: pd.DataFrame, save_mock_filters: bool = False):
         yield movie_data
 
 
-if __name__ == "__main__":
+def load_movie_data(rows: int | None = None) -> pd.DataFrame:
     in_file_name = "TMDB_movie_dataset.csv"
     if in_file_name not in os.listdir():
         raise FileNotFoundError(
             f"{in_file_name} not found. Download it from {DOWNLOAD_DATA_URL} and unzip it in the same directory as this script with the correct filename."
         )
 
-    MOVIE_COUNT = 50
     df = pd.read_csv(in_file_name)
     df.dropna(
         subset=["id", "title", "release_date", "overview", "runtime"], inplace=True
     )  # Drop rows with missing values in these columns, as they are required
-    df = df.head(MOVIE_COUNT)
+    df.drop_duplicates(subset=["id"], inplace=True)
+    if rows is not None:
+        df = df.head(rows)
     print("Done reading CSV")
+    return df
+
+
+def create_mock_data(rows: int = 50):
+    df = load_movie_data(rows)
 
     all_movies = get_all_movies(df, save_mock_filters=True)
 
     with open("mock_movies.json", "w") as f:
         json.dump(list(all_movies), f, indent=4)
     print("Data written to mock_movies.json")
+
+
+def fill_db(rows: int | None = None):
+    DB_NAME = "T26-Project-2"
+    MOVIES_COL_NAME = "movies"
+
+    CLIENT = pymongo.MongoClient("mongodb://localhost:27017/")
+    DB = CLIENT[DB_NAME]
+    MOVIES_COL = DB[MOVIES_COL_NAME]
+
+    if MOVIES_COL.count_documents({}) > 0:
+        print("Database already has data. First drop all data.")
+        return
+
+    df = load_movie_data(rows)
+    all_movies = get_all_movies(df, save_mock_filters=False)
+    for movie in tqdm(all_movies, total=len(df.index)):
+        MOVIES_COL.insert_one(movie)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 parse_data.py mock|db <rows>")
+        print("(Set rows to 0 for all rows)")
+        sys.exit(1)
+
+    if not sys.argv[2].isdigit():
+        print("Rows must be an integer")
+        sys.exit(1)
+
+    rows = int(sys.argv[2]) if sys.argv[2] != "0" else None
+    if sys.argv[1] == "mock":
+        create_mock_data(rows)
+    elif sys.argv[1] == "db":
+        fill_db(rows)
