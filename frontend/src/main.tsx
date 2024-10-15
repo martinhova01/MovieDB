9b48cfd69ca63ce517854e7da6c6a8df88935e6a
Runar Saur Modahl <runarsm@stud.ntnu.no>
2024-10-15 19:19:59 +0200

    feat(frontend): add basic Apollo Client setup for querying backend (#24)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,29 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
+const client = new ApolloClient({
+    uri: "http://localhost:3001/",
+    cache: new InMemoryCache(),
+});
+
+// Example query using Apollo Client (backend server must be running)
+// Read more https://www.apollographql.com/docs/react/get-started
+client
+    .query({
+        query: gql`
+            query GetMovies {
+                movies(skip: 10, limit: 3) {
+                    title
+                    tagline
+                    release_date
+                }
+            }
+        `,
+    })
+    .then((result) => console.log(result));
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
