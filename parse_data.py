58fbcca5300ce440ba68ba081231f86fe3c38c9c
martin Høva <martirho@stud.ntnu.no>
2024-10-22 08:30:07 +0200

    fix: pep8 violation (#31)

--- parse_data.py
+++ parse_data.py
@@ -99,7 +99,7 @@ def load_movie_data(rows: int | None = None) -> pd.DataFrame:
     df.dropna(
         subset=["id", "title", "release_date", "overview", "runtime"], inplace=True
     )  # Drop rows with missing values in these columns, as they are required
-    df = df[df["adult"] == False]
+    df = df[~df["adult"]]
     df.rename({"id": "_id"}, axis=1, inplace=True)
     df.drop_duplicates(subset=["_id"], inplace=True)
     if rows is not None:
