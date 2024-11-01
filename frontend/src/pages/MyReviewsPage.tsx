591d8c38d9a2fda7f4cca9d3fab10114100025ab
Petter Teisberg <pteisb@gmail.com>
2024-11-01 17:41:08 +0100

    fix: refetch reviews on username change to prevent incorrect owner assignment (#37)

--- frontend/src/pages/MyReviewsPage.tsx
+++ frontend/src/pages/MyReviewsPage.tsx
@@ -1,6 +1,6 @@
 import ReviewCard from "@/components/ReviewCard";
 import { Button } from "@/shadcn/components/ui/button";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import { useQuery, useReactiveVar } from "@apollo/client";
 import { GET_USER_REVIEWS } from "../api/queries";
@@ -12,21 +12,28 @@ const MyReviewsPage = () => {
     const [isMoreReviews, setIsMoreReviews] = useState<boolean>(true);
     const LIMIT = 20;
 
-    const { data, loading, error, fetchMore } = useQuery(GET_USER_REVIEWS, {
-        variables: {
-            username: username,
-            skip: 0,
-            limit: LIMIT,
-        },
-        onCompleted: (data) => {
-            if (data.userReviews) {
-                setIsMoreReviews(
-                    data.userReviews.length >= LIMIT &&
-                        data.userReviews.length % LIMIT === 0
-                );
-            }
-        },
-    });
+    const { data, loading, error, fetchMore, refetch } = useQuery(
+        GET_USER_REVIEWS,
+        {
+            variables: {
+                username: username,
+                skip: 0,
+                limit: LIMIT,
+            },
+            onCompleted: (data) => {
+                if (data.userReviews) {
+                    setIsMoreReviews(
+                        data.userReviews.length >= LIMIT &&
+                            data.userReviews.length % LIMIT === 0
+                    );
+                }
+            },
+        }
+    );
+
+    useEffect(() => {
+        refetch();
+    }, [username, refetch]);
 
     const userReviews = useMemo(
         () => data?.userReviews as Review[] | undefined,
