ed5bf6824c4aaa38d585f21bf74ebc895bbf14cf
Runar Saur Modahl <runarsm@stud.ntnu.no>
2024-11-05 14:54:59 +0100

    feat: add use-debounce for improved search input handling (#59)

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,28 +1,31 @@
 import { searchVar } from "@/utils/cache";
-import { Button } from "@/shadcn/components/ui/button";
 import { Input } from "@/shadcn/components/ui/input";
-import { Search } from "lucide-react";
 import React, { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 const SearchBar: React.FC = () => {
     const [search, setSearch] = useState<string>(searchVar());
 
     const handleSearch = (searchString: string) => {
+        searchString = searchString.trim();
         sessionStorage.setItem("search", searchString);
         searchVar(searchString);
     };
 
+    const debouncedHandleSearch = useDebouncedCallback(handleSearch, 510);
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
+        debouncedHandleSearch(e.target.value);
         if (!e.target.value) {
-            handleSearch(e.target.value.trim());
+            debouncedHandleSearch.flush();
         }
     };
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         const target = e.target as HTMLInputElement;
         if (e.key === "Enter" && target.value.trim()) {
-            handleSearch(target.value.trim());
+            debouncedHandleSearch.flush();
         }
     };
 
@@ -38,13 +41,6 @@ const SearchBar: React.FC = () => {
                 onKeyDown={handleKeyDown}
                 aria-label="Search for movies"
             />
-            <Button
-                type="submit"
-                onClick={() => handleSearch(search.trim())}
-                aria-label="Search"
-            >
-                <Search />
-            </Button>
         </section>
     );
 };
