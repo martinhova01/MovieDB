a31d3329272061cee019329a4f6be6662286b082
martin Høva <martirho@stud.ntnu.no>
2024-11-13 13:43:48 +0100

    test: add test for deleteReview in ReviewCard (#65)

--- frontend/src/test/ReviewCard.test.tsx
+++ frontend/src/test/ReviewCard.test.tsx
@@ -1,20 +1,39 @@
 import { Review } from "@/types/__generated__/types";
 import { all_reviews } from "./mock/util";
 import { usernameVar } from "@/utils/cache";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ReviewCard from "@/components/ReviewCard";
 import { MockedProvider } from "@apollo/client/testing";
 import { MemoryRouter } from "react-router-dom";
 import { formatDate } from "@/utils/formatUtil";
+import { DELETE_REVIEW } from "@/api/queries";
 
 const mockReview: Review = all_reviews[0];
+
+const mockDeleteReview = [
+    {
+        request: {
+            query: DELETE_REVIEW,
+            variables: {
+                id: "67345815fd0434382c87e27d",
+            },
+        },
+        result: {
+            data: {
+                deleteReview: {
+                    ...all_reviews[0],
+                },
+            },
+        },
+    },
+];
 
 describe("ReviewCard", () => {
     const renderComponent = (review: Review, showPoster = false) => {
         return render(
             <MemoryRouter>
-                <MockedProvider mocks={[]} addTypename={false}>
+                <MockedProvider mocks={mockDeleteReview} addTypename={false}>
                     <ReviewCard review={review} showPoster={showPoster} />
                 </MockedProvider>
             </MemoryRouter>
@@ -61,6 +80,27 @@ describe("ReviewCard", () => {
         renderComponent(mockReview);
         expect(
             screen.queryByRole("button", { name: /delete/i })
+        ).not.toBeInTheDocument();
+    });
+
+    it("shows dialog box when 'delete' is clicked", async () => {
+        usernameVar("test_user");
+        renderComponent(mockReview);
+
+        const deleteButton = screen.getByRole("button", { name: /delete/i });
+        fireEvent.click(deleteButton);
+        //AlertDialog opens
+        expect(
+            await screen.findByText(
+                "Are you sure you want to delete this review?"
+            )
+        ).toBeInTheDocument();
+
+        const continuButton = screen.getByRole("button", { name: /continue/i });
+        fireEvent.click(continuButton);
+        //AlertDialog closes
+        expect(
+            screen.queryByText("Are you sure you want to delete this review?")
         ).not.toBeInTheDocument();
     });
 });
