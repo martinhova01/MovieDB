66c349eb98995093803bd70a48ece8233c4a3d77
Runar Saur Modahl <runarsm@stud.ntnu.no>
2024-11-14 18:30:59 +0100

    test: set up in-memory MongoDB for database query testing (#69)

--- backend/src/db/database.ts
+++ backend/src/db/database.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
-async function connectToDatabase() {
+async function connectToDatabase(
+    uri: string = "mongodb://localhost:27017/T26-Project-2"
+) {
     try {
-        await mongoose.connect("mongodb://localhost:27017/T26-Project-2");
+        await mongoose.connect(uri);
         console.log("Successfully connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB\n", error);
